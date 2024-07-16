d344a2e1eb299566ac81cb5ff2a1a25681c6d1e1
JordyCA <jordycastroavendano@gmail.com>
2024-07-16 13:01:54 -0600

    second part redux thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -6,25 +6,27 @@ import {
 import { useSelector } from 'react-redux';
 //** Components */
 import CategoryPreview from '../../components/category-preview/category-preview.component.jsx';
+import Spinner from '../../components/spinner/spinner.component.jsx';
 
 //** Contexts */
 // import { CategoriesContext } from '../../contexts/categories.context.jsx';
 
 //** Redux */
-import {selectCategoriesMap} from '../../store/categories/category.selector.jsx';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector.jsx';
 
 const CategoriesPreview = () => {
-    // const { categoriesMap } = useContext(CategoriesContext);
-    const categoriesMap = useSelector(selectCategoriesMap);    
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     return (
         <Fragment>
             {
-                Object.keys(categoriesMap).map((title) => {
-                    const products = categoriesMap[title];
-                    return (
-                        <CategoryPreview key={title} title={title} products={products} />
-                    );
-                })
+                isLoading ? <Spinner /> :
+                    Object.keys(categoriesMap).map((title) => {
+                        const products = categoriesMap[title];
+                        return (
+                            <CategoryPreview key={title} title={title} products={products} />
+                        );
+                    })
             }
         </Fragment>
     );
